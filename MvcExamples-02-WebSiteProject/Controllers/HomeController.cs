a952906293002f05a928632fa369ff35ca3f8f61
Samet Alabey <alabeysamet@gmail.com>
2019-01-28 15:10:54 +0300

    Siteye tema giydirme.

---
+++ MvcExamples-02-WebSiteProject/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MvcExamples_02_WebSiteProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcExamples-02-WebSiteProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcExamples_02_WebSiteProject.MvcApplication" Language="C#" %>
