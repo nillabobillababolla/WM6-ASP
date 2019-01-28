ab3e02b3c12ec9f2993f70fe858106157d420965
Samet Alabey <alabeysamet@gmail.com>
2019-01-28 14:17:00 +0300

    PartialView

---
+++ MvcExamples-01-Layout/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MvcExamples_01_Layout.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+
+        public PartialViewResult UrunGetir()
+        {
+            List<string> urunListesi = new List<string>
+            {
+                "Kalem",
+                "Silgi",
+                "Defter"
+            };
+            return PartialView("~/Views/Home/_partialUrunListesi.cshtml", urunListesi);
+        }
+
+    }
+
+}
---
+++ MvcExamples-01-Layout/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcExamples_01_Layout
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
