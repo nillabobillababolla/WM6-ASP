f9f69d61833a2ed19437c0c4c0465b513fd9b100
Samet Alabey <alabeysamet@gmail.com>
2019-01-28 09:09:58 +0300

    Add project files.

---
+++ AjaxIslemler/Controllers/CategoryController.cs
@@ -0,0 +1,168 @@
+﻿using AjaxIslemler.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using AjaxIslemler.Models.ViewModels;
+
+namespace AjaxIslemler.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public JsonResult Search(string s)
+        {
+            var key = s.ToLower();
+            if (key.Length <= 2 && key != "*")
+            {
+                return Json(new ResponseData()
+                {
+                    message = "Aramak icin 2 karakterden fazlasini girin",
+                    success = false
+                }, JsonRequestBehavior.AllowGet);
+            }
+            try
+            {
+                var db = new NorthwindEntities();
+                List<CategoryViewModel> data;
+                if (key == "*")
+                {
+                    data = db.Categories.OrderBy(x => x.CategoryName)
+                        .Select(x => new CategoryViewModel()
+                        {
+                            CategoryName = x.CategoryName,
+                            Description = x.Description,
+                            CategoryID = x.CategoryID,
+                            ProductCount = x.Products.Count
+                        }).ToList();
+                }
+                else
+                {
+                    data = db.Categories
+                        .Where(x =>
+                            x.CategoryName.ToLower().Contains(key)
+                            || x.Description.Contains(key))
+                        .Select(x => new CategoryViewModel()
+                        {
+                            CategoryName = x.CategoryName,
+                            Description = x.Description,
+                            CategoryID = x.CategoryID,
+                            ProductCount = x.Products.Count
+                        })
+                        .ToList();
+                }
+                return Json(new ResponseData()
+                {
+                    message = $"{data.Count} adet kayit bulundu",
+                    success = true,
+                    data = data
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new ResponseData()
+                {
+                    message = $"Bir hata olustu {ex.Message}",
+                    success = false
+                }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Add(CategoryViewModel model)
+        {
+            try
+            {
+                var db = new NorthwindEntities();
+                db.Categories.Add(new Category()
+                {
+                    CategoryName = model.CategoryName,
+                    Description = model.Description
+                });
+                db.SaveChanges();
+                return Json(new ResponseData()
+                {
+                    message = $"{model.CategoryName} ismindeki kategori basariyla eklendi",
+                    success = true
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new ResponseData()
+                {
+                    message = $"Bir hata olustu {ex.Message}",
+                    success = false
+                }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Delete(int id)
+        {
+            try
+            {
+                var db = new NorthwindEntities();
+                var cat = db.Categories.Find(id);
+                db.Categories.Remove(entity: cat ?? throw new InvalidOperationException());
+                db.SaveChanges();
+                return Json(new ResponseData()
+                {
+                    message = $"{cat.CategoryName} ismindeki kategori basariyla silindi",
+                    success = true
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new ResponseData()
+                {
+                    message = $"Kategori silme isleminde hata {ex.Message}",
+                    success = false
+                }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Update(Category model)
+        {
+            try
+            {
+                var db = new NorthwindEntities();
+                var cat = db.Categories.Find(model.CategoryID);
+                if (cat == null)
+                {
+                    return Json(new ResponseData()
+                    {
+                        message = $"Kategori bulunamadi",
+                        success = false
+                    }, JsonRequestBehavior.AllowGet);
+                }
+                cat.Description = model.Description;
+                cat.CategoryName = model.CategoryName;
+                db.SaveChanges();
+                return Json(new ResponseData()
+                {
+                    message = $"{cat.CategoryName} ismindeki kategori basariyla guncellendi",
+                    success = true
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new ResponseData()
+                {
+                    message = $"Kategori guncelleme isleminde hata {ex.Message}",
+                    success = false
+                }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult Detail()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IlkMvcSayfam/Controllers/CategoryController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using IlkMvcSayfam.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace IlkMvcSayfam.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var data = new NorthwindEntities()
+                .Categories
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+            return View(data);
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+
+            var data = new NorthwindEntities().Categories.Find(id.Value);
+            if (data == null) RedirectToAction("Index");
+
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Add(Category category)
+        {
+            try
+            {
+                var db = new NorthwindEntities();
+                db.Categories.Add(category);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            var db = new NorthwindEntities();
+            try
+            {
+                var category = db.Categories.Find(id.GetValueOrDefault());
+                if (category == null) return RedirectToAction("Index");
+
+                db.Categories.Remove(category);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Update(int? id)
+        {
+            if (id == null)
+                return RedirectToAction("Index", "Category");
+
+            try
+            {
+                var data = new NorthwindEntities().Categories.Find(id.Value);
+                if (data == null)
+                    return RedirectToAction("Index", "Category");
+
+                return View(data);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Category");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Update(Category model)
+        {
+            try
+            {
+                var db = new NorthwindEntities();
+                var data = db.Categories.Find(model.CategoryID);
+
+                if (data == null)
+                    return RedirectToAction("Index");
+
+                data.CategoryName = model.CategoryName;
+                data.Description = model.Description;
+                db.SaveChanges();
+                ViewBag.Message = "<span class='text text-success'>Update Successfully</span>";
+                return View(data);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = $"<span class='text text-danger'>Update Error {ex.Message}</span>";
+                return View(model);
+            }
+        }
+
+        public JsonResult Categories()
+        {
+            var categoriler = new NorthwindEntities().Categories.Select(x => new
+            {
+                x.CategoryName,
+                x.CategoryID,
+                x.Description,
+                ProductCount = x.Products.Count
+            }).ToList();
+
+            return Json(categoriler, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
