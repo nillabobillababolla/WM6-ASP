dfc00a7d28fd570696dec0fdefb57644999233d9
Samet Alabey <alabeysamet@gmail.com>
2019-01-29 11:26:15 +0300

    Partial kısımlar eklendi.

---
+++ Admin.Web.UI/Controllers/PartialController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Admin.Web.UI.Controllers
+{
+    public class PartialController : Controller
+    {
+        public PartialViewResult DrawerPartial()
+        {
+            var data = new List<string>();
+            return PartialView("Partial/_DrawerPartial",data);
+        }
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView("Partial/_HeaderPartial");
+        }
+        public PartialViewResult ModalPartial()
+        {
+            return PartialView("Partial/_ModalPartial");
+        }
+    }
+}
