3db79c6b55d9ae037662e3b66becd032ac6c1676
Samet Alabey <alabeysamet@gmail.com>
2019-02-04 14:17:13 +0300

    Login&Logout done.

--- Admin.Web.UI/Controllers/AccountController.cs
+++ Admin.Web.UI/Controllers/AccountController.cs
@@ -1,9 +1,12 @@
-﻿using Admin.BLL.Identity;
-using Admin.Models.Identity.Models;
+﻿using Admin.Models.Identity.Models;
 using Admin.Models.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
 using static Admin.BLL.Identity.MembershipTools;
 
@@ -14,6 +17,11 @@ namespace Admin.Web.UI.Controllers
         [HttpGet]
         public ActionResult Index()
         {
+            if (HttpContext.GetOwinContext().Authentication.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
             return View();
         }
 
@@ -25,29 +33,28 @@ namespace Admin.Web.UI.Controllers
             {
                 return View("Index", model);
             }
-           
             try
             {
-                var userStore = NewUserStore();
-                var rm = model.RegisterViewModel;
-                var userManager = NewUserManager();
-                var user = await userManager.FindByNameAsync(rm.UserName);
+                UserStore<User> userStore = NewUserStore();
+                RegisterViewModel rm = model.RegisterViewModel;
+                Microsoft.AspNet.Identity.UserManager<User> userManager = NewUserManager();
+                User user = await userManager.FindByNameAsync(rm.UserName);
                 if (user != null)
                 {
                     ModelState.AddModelError("UserName", "Bu kullanici adi daha önceden alınmıştır.");
                     return View("Index", model);
                 }
-                var newUser = new User()
+                User newUser = new User()
                 {
                     Name = rm.UserName,
-                    Email =rm.Email,
-                    UserName =rm.UserName,
-                    Surname=rm.Surname
+                    Email = rm.Email,
+                    UserName = rm.UserName,
+                    Surname = rm.Surname
                 };
-                var result = await userManager.CreateAsync(newUser, rm.Password);
+                Microsoft.AspNet.Identity.IdentityResult result = await userManager.CreateAsync(newUser, rm.Password);
                 if (result.Succeeded)
                 {
-                    if (userStore.Users.Count()==1)
+                    if (userStore.Users.Count() == 1)
                     {
                         await userManager.AddToRoleAsync(newUser.Id, "Admin");
                     }
@@ -58,8 +65,8 @@ namespace Admin.Web.UI.Controllers
                 }
                 else
                 {
-                    var err = "";
-                    foreach (var resultError in result.Errors)
+                    string err = "";
+                    foreach (string resultError in result.Errors)
                     {
                         err += resultError + " ";
                     }
@@ -80,6 +87,52 @@ namespace Admin.Web.UI.Controllers
                 };
                 return RedirectToAction("Error", "Home");
             }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(RegisterLoginViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View("Index", model);
+
+                var userManager = NewUserManager();
+                var user = await userManager.FindAsync(model.LoginViewModel.UserName, model.LoginViewModel.Password);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Kullanıcı adı veya şifre hatalı");
+                    return View("Index", model);
+                }
+                var authManager = HttpContext.GetOwinContext().Authentication;
+                var userIdentity =
+                    await userManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
+                authManager.SignIn(new AuthenticationProperties()
+                {
+                    IsPersistent = model.LoginViewModel.RememberMe
+                }, userIdentity);
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                TempData["Model"] = new ErrorViewModel()
+                {
+                    Text = $"Bir hata olustu. {ex.Message}",
+                    ActionName = "Index",
+                    ControllerName = "Account",
+                    ErrorCode = 500
+                };
+                return RedirectToAction("Error", "Home");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            Microsoft.Owin.Security.IAuthenticationManager authManager = HttpContext.GetOwinContext().Authentication;
+            authManager.SignOut();
+            return RedirectToAction("Index");
         }
     }
 }
