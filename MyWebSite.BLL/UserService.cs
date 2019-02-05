b526014e363ba4a65d9b110c2288cbeddc40676c
Samet Alabey <alabeysamet@gmail.com>
2019-02-05 13:32:02 +0300

    MyWebSite

---
+++ MyWebSite.BLL/UserService.cs
@@ -0,0 +1,18 @@
+﻿using MyWebSite.DAL.MyEntities;
+using MyWebSite.DAL.Repositories.Abstract;
+
+namespace MyWebSite.BLL
+{
+    public class UserService
+    {
+        UserRepository _userRepository;
+        public UserService()
+        {
+            _userRepository = new UserRepository();
+        }
+        public int AddUser(User item)
+        {
+            return _userRepository.AddItem(item);
+        }
+    }
+}
