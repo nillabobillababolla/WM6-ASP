bca18c4553fdb1407e2c4929a0dde5d1280c95f1
Samet Alabey <alabeysamet@gmail.com>
2019-02-06 14:45:31 +0300

    userprofile

--- Admin.Web.UI/Controllers/AccountController.cs
+++ Admin.Web.UI/Controllers/AccountController.cs
@@ -1,14 +1,13 @@
-﻿using Admin.Models.Identity.Models;
-using Admin.Models.ViewModels;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.Owin.Security;
+﻿using Admin.Models.ViewModels;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
 using static Admin.BLL.Identity.MembershipTools;
+using Admin.Models.Identity.Models;
 
 namespace Admin.Web.UI.Controllers
 {
@@ -18,10 +17,7 @@ namespace Admin.Web.UI.Controllers
         public ActionResult Index()
         {
             if (HttpContext.GetOwinContext().Authentication.User.Identity.IsAuthenticated)
-            {
                 return RedirectToAction("Index", "Home");
-            }
-
             return View();
         }
 
@@ -35,24 +31,26 @@ namespace Admin.Web.UI.Controllers
             }
             try
             {
-                UserStore<User> userStore = NewUserStore();
-                RegisterViewModel rm = model.RegisterViewModel;
-                Microsoft.AspNet.Identity.UserManager<User> userManager = NewUserManager();
-                User user = await userManager.FindByNameAsync(rm.UserName);
+                var userStore = NewUserStore();
+                var userManager = NewUserManager();
+
+                var rm = model.RegisterViewModel;
+
+                var user = await userManager.FindByNameAsync(rm.UserName);
                 if (user != null)
                 {
-                    ModelState.AddModelError("UserName", "Bu kullanici adi daha önceden alınmıştır.");
+                    ModelState.AddModelError("UserName", "Bu kullanıcı adı daha önceden alınmıştır");
                     return View("Index", model);
                 }
-                User newUser = new User()
+
+                var newUser = new User()
                 {
-                    Name = rm.UserName,
-                    Email = rm.Email,
                     UserName = rm.UserName,
+                    Email = rm.Email,
+                    Name = rm.Name,
                     Surname = rm.Surname
                 };
-
-                IdentityResult result = await userManager.CreateAsync(newUser, rm.Password);
+                var result = await userManager.CreateAsync(newUser, rm.Password);
                 if (result.Succeeded)
                 {
                     if (userStore.Users.Count() == 1)
@@ -63,25 +61,27 @@ namespace Admin.Web.UI.Controllers
                     {
                         await userManager.AddToRoleAsync(newUser.Id, "User");
                     }
+                    //todo kullanıcıya mail gönderilsin
                 }
                 else
                 {
-                    string err = "";
-                    foreach (string resultError in result.Errors)
+                    var err = "";
+                    foreach (var resultError in result.Errors)
                     {
                         err += resultError + " ";
                     }
                     ModelState.AddModelError("", err);
                     return View("Index", model);
                 }
-                TempData["Message"] = "Kaydınız alınmıştır. Lütfen giriş yapınız";
+
+                TempData["Message"] = "Kaydınız alınlıştır. Lütfen giriş yapınız";
                 return RedirectToAction("Index");
             }
             catch (Exception ex)
             {
                 TempData["Model"] = new ErrorViewModel()
                 {
-                    Text = $"Bir hata olustu. {ex.Message}",
+                    Text = $"Bir hata oluştu {ex.Message}",
                     ActionName = "Index",
                     ControllerName = "Account",
                     ErrorCode = 500
@@ -97,19 +97,17 @@ namespace Admin.Web.UI.Controllers
             try
             {
                 if (!ModelState.IsValid)
-                {
                     return View("Index", model);
-                }
 
-                UserManager<User> userManager = NewUserManager();
-                User user = await userManager.FindAsync(model.LoginViewModel.UserName, model.LoginViewModel.Password);
+                var userManager = NewUserManager();
+                var user = await userManager.FindAsync(model.LoginViewModel.UserName, model.LoginViewModel.Password);
                 if (user == null)
                 {
                     ModelState.AddModelError("", "Kullanıcı adı veya şifre hatalı");
                     return View("Index", model);
                 }
-                IAuthenticationManager authManager = HttpContext.GetOwinContext().Authentication;
-                System.Security.Claims.ClaimsIdentity userIdentity =
+                var authManager = HttpContext.GetOwinContext().Authentication;
+                var userIdentity =
                     await userManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
                 authManager.SignIn(new AuthenticationProperties()
                 {
@@ -121,7 +119,7 @@ namespace Admin.Web.UI.Controllers
             {
                 TempData["Model"] = new ErrorViewModel()
                 {
-                    Text = $"Bir hata olustu. {ex.Message}",
+                    Text = $"Bir hata oluştu {ex.Message}",
                     ActionName = "Index",
                     ControllerName = "Account",
                     ErrorCode = 500
@@ -133,22 +131,19 @@ namespace Admin.Web.UI.Controllers
         [HttpGet]
         public ActionResult Logout()
         {
-            IAuthenticationManager authManager = HttpContext.GetOwinContext().Authentication;
+            var authManager = HttpContext.GetOwinContext().Authentication;
             authManager.SignOut();
-            return RedirectToAction("Index");
+            return RedirectToAction("Index", "Account");
         }
 
         [HttpGet]
         [Authorize]
-        public async Task<ActionResult> UserProfile()
+        public ActionResult UserProfile()
         {
-            var id = HttpContext.GetOwinContext().Authentication.User.Identity.GetUserId();
-
             try
             {
-                var userManager = NewUserManager();
-                var user = await userManager.FindByIdAsync(id);
-
+                var id = HttpContext.GetOwinContext().Authentication.User.Identity.GetUserId();
+                var user = NewUserManager().FindById(id);
                 var data = new ProfilePasswordViewModel()
                 {
                     UserProfileViewModel = new UserProfileViewModel()
@@ -156,8 +151,8 @@ namespace Admin.Web.UI.Controllers
                         Email = user.Email,
                         Id = user.Id,
                         Name = user.Name,
-                        Surname = user.Surname,
                         PhoneNumber = user.PhoneNumber,
+                        Surname = user.Surname,
                         UserName = user.UserName
                     }
                 };
@@ -176,7 +171,6 @@ namespace Admin.Web.UI.Controllers
             }
         }
 
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize]
@@ -194,15 +188,78 @@ namespace Admin.Web.UI.Controllers
 
                 user.Name = model.UserProfileViewModel.Name;
                 user.Surname = model.UserProfileViewModel.Surname;
+                user.PhoneNumber = model.UserProfileViewModel.PhoneNumber;
                 if (user.Email != model.UserProfileViewModel.Email)
                 {
-                    //todo tekrardan aktivasyon gönderilmeli ve kullanıcı rolü aktif olamayan bir kullanıcı rolüne atanmalı!
+                    //todo tekrar aktivasyon maili gönderilmeli. rolü de aktif olmamış role çevrilmeli.
                 }
                 user.Email = model.UserProfileViewModel.Email;
-                user.PhoneNumber = model.UserProfileViewModel.PhoneNumber;
+
                 await userManager.UpdateAsync(user);
-                TempData["Message"] = "Profiliniz güncellenmiştir";
+                TempData["Message"] = "Güncelleme işlemi başarılı";
                 return RedirectToAction("UserProfile");
+            }
+            catch (Exception ex)
+            {
+                TempData["Model"] = new ErrorViewModel()
+                {
+                    Text = $"Bir hata oluştu {ex.Message}",
+                    ActionName = "UserProfile",
+                    ControllerName = "Account",
+                    ErrorCode = 500
+                };
+                return RedirectToAction("Error", "Home");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<ActionResult> ChangePassword(ProfilePasswordViewModel model)
+        {
+            try
+            {
+                var userManager = NewUserManager();
+                var id = HttpContext.GetOwinContext().Authentication.User.Identity.GetUserId();
+                var user = NewUserManager().FindById(id);
+                var data = new ProfilePasswordViewModel()
+                {
+                    UserProfileViewModel = new UserProfileViewModel()
+                    {
+                        Email = user.Email,
+                        Id = user.Id,
+                        Name = user.Name,
+                        PhoneNumber = user.PhoneNumber,
+                        Surname = user.Surname,
+                        UserName = user.UserName
+                    }
+                };
+                model.UserProfileViewModel = data.UserProfileViewModel;
+                if (!ModelState.IsValid)
+                {
+                    return View("UserProfile", model);
+                }
+
+
+                var result = await userManager.ChangePasswordAsync(
+                    HttpContext.GetOwinContext().Authentication.User.Identity.GetUserId(),
+                    model.ChangePasswordViewModel.OldPassword, model.ChangePasswordViewModel.NewPassword);
+
+                if (result.Succeeded)
+                {
+                    //todo kullanıcıyı bilgilendiren bir mail atılır
+                    return RedirectToAction("Logout", "Account");
+                }
+                else
+                {
+                    var err = "";
+                    foreach (var resultError in result.Errors)
+                    {
+                        err += resultError + " ";
+                    }
+                    ModelState.AddModelError("", err);
+                    return View("UserProfile", model);
+                }
             }
             catch (Exception ex)
             {
