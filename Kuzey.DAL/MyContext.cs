ee566705e38cc3212b18e7df74e7f51525c36a56
Samet Alabey <alabeysamet@gmail.com>
2019-02-12 10:23:32 +0300

    Product,Category,Employee nesneleri için mapping eklendi.

---
+++ Kuzey.DAL/MyContext.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Kuzey.Models.Entities
+{
+    public partial class MyContext : DbContext
+    {
+        public MyContext()
+            : base("name=MyCon")
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<CustomerDemographic> CustomerDemographics { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<Order_Detail> Order_Details { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Shipper> Shippers { get; set; }
+        public virtual DbSet<Supplier> Suppliers { get; set; }
+        public virtual DbSet<Territory> Territories { get; set; }
+        public DateTime InstanceDate { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CustomerDemographic>()
+                .Property(e => e.CustomerTypeID)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Customer>()
+                .Property(e => e.CustomerID)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Employee>()
+                .HasMany(e => e.Employees1)
+                .WithOptional(e => e.Employee1)
+                .HasForeignKey(e => e.ReportsTo);
+
+            modelBuilder.Entity<Employee>()
+                .HasMany(e => e.Territories)
+                .WithMany(e => e.Employees)
+                .Map(m => m.ToTable("EmployeeTerritories").MapLeftKey("EmployeeID").MapRightKey("TerritoryID"));
+
+            modelBuilder.Entity<Order_Detail>()
+                .Property(e => e.UnitPrice)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Order>()
+                .Property(e => e.CustomerID)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Order>()
+                .Property(e => e.Freight)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Order>()
+                .HasMany(e => e.Order_Details)
+                .WithRequired(e => e.Order)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.UnitPrice)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(e => e.Order_Details)
+                .WithRequired(e => e.Product)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Shipper>()
+                .HasMany(e => e.Orders)
+                .WithOptional(e => e.Shipper)
+                .HasForeignKey(e => e.ShipVia);
+
+            modelBuilder.Entity<Territory>()
+                .Property(e => e.TerritoryDescription)
+                .IsFixedLength();
+        }
+    }
+}
---
+++ Kuzey.Web/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Kuzey.BLL.Repository;
+using Kuzey.Models.Entities;
+using Kuzey.Models.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Kuzey.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var data = new CategoryRepo().GetAll().Select(x => Mapper.Map<CategoryViewModel>(x)).ToList();
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Add(CategoryViewModel model)
+        {
+            new CategoryRepo().Insert(Mapper.Map<CategoryViewModel, Category>(model));
+            return View();
+        }
+    }
+}
