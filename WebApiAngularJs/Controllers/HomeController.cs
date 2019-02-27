665cce47d200704f7dfb8f211ca632821d6bda11
Samet Alabey <alabeysamet@gmail.com>
2019-02-27 14:54:13 +0300

    AngularJs giriş.
    angular js ile basit işlemler.

---
+++ WebApiAngularJs/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace WebApiAngularJs.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Products()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApiAngularJs/Scripts/app.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="angular.js" />
+
+var app = angular.module("myApp", []);
+
+app.controller("ProductCtrl", function ($scope) {
+    $scope.urunler = [];
+
+    $scope.ekle = function () {
+        $scope.urunler.push({
+            urunAdi: $scope.yeni.urunAdi,
+            fiyat: $scope.yeni.fiyat,
+            eklenmeZamani: new Date()
+        });
+    };
+});
