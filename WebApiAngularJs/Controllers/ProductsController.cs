3389b628025b66a29d241fd0ae84e6046001d8ca
Samet Alabey <alabeysamet@gmail.com>
2019-03-04 08:49:37 +0300

    Northwind ile AngularJs işlemleri

---
+++ WebApiAngularJs/Controllers/ProductsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApiAngularJs.Models;
+
+namespace WebApiAngularJs.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private NorthWindEntities db = new NorthWindEntities();
+
+        // GET: api/Products
+        public IQueryable<Product> GetProducts()
+        {
+            return db.Products;
+        }
+
+        // GET: api/Products/5
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            try
+            {
+                return Ok(new
+                {
+                    success = true,
+                    data = db.Products.Select(x => new ProductViewModel()
+                    {
+                        ProductID = x.ProductID,
+                        ProductName = x.ProductName,
+                        UnitPrice = x.UnitPrice,
+                        UnitsInStock = x.UnitsInStock
+                    }).ToList()
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Bir hata olustu.{ex.Message}");
+            }
+        }
+
+        // PUT: api/Products/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutProduct(int id, Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != product.ProductID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Products
+        [ResponseType(typeof(Product))]
+        public async Task<IHttpActionResult> PostProduct(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Products.Add(product);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = product.ProductID }, product);
+        }
+
+        // DELETE: api/Products/5
+        [ResponseType(typeof(Product))]
+        public async Task<IHttpActionResult> DeleteProduct(int id)
+        {
+            Product product = await db.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            db.Products.Remove(product);
+            await db.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductExists(int id)
+        {
+            return db.Products.Count(e => e.ProductID == id) > 0;
+        }
+    }
+
+    public class ProductViewModel{
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public short? UnitsInStock { get; set; }
+    }
+}
--- WebApiAngularJs/Scripts/app.js
+++ WebApiAngularJs/Scripts/app.js
@@ -1,54 +1,26 @@
 ﻿/// <reference path="angular.js" />
-
+var host = 'http://localhost:63006/';
 var app = angular.module("myApp", []);
 
-app.controller("ProductCtrl", function ($scope) {
-    $scope.urunler = [];
-    $scope.sepet = [];
-    function init() {
-        var data = JSON.parse(localStorage.getItem("urunler"));
-        $scope.urunler = data === null ? [] : data;
-    }
-    $scope.ekle = function () {
-        $scope.urunler.push({
-            id: guid(),
-            urunAdi: $scope.yeni.urunAdi,
-            fiyat: $scope.yeni.fiyat,
-            eklenmeZamani: new Date()
-        });
-        $scope.yeni.urunAdi = "";
-        $scope.yeni.fiyat = "";
-        localStorage.setItem("urunler", JSON.stringify($scope.urunler));
-    };
+app.controller("ProductCtrl", function ($scope, $http) {
+    $scope.productList = [];
 
-    $scope.sil = function (id) {
-        for (var i = 0; i < $scope.urunler.length; i++) {
-            var data = $scope.urunler[i];
-            if (id === data.id) {
-                $scope.urunler.splice(i, 1);
-                break;
+    function init() {
+        $http({
+            method: "GET",
+            url: host + "/api/product/getall"
+        }).then(function (rs) {
+            console.log(rs);
+            if (rs.data.success) {
+                $scope.productList = rs.data.data;
+            } else {
+                alert("bir hata oluştu " + rs.data.message);
             }
         }
-        localStorage.setItem("urunler", JSON.stringify($scope.urunler));
-    };
+    
 
-    $scope.sepetekle = function (id) {
-        $scope.sepet.push({
-            id: id,
-            urunAdi: $scope.yeni.urunAdi,
-            fiyat: $scope.yeni.fiyat,
-            eklenmeZamani: new Date()
-        });
-    };
-
-    function guid() {
-        function S4() {
-            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-        }
-
-        // then to call it, plus stitch in '4' in the third group
-        return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0, 3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+    $scope.getall = function () {
+       
     }
-
     init();
 });
