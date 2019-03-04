6d5cc11ab223aff2dbe0a3d58de60ced6ed75248
Samet Alabey <alabeysamet@gmail.com>
2019-03-04 11:35:53 +0300

    Mesajlaşma uygulaması with SignalR.

---
+++ SignalRApp/App_Start/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRApp.App_Start.Startup))]
+namespace SignalRApp.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ SignalRApp/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+
+namespace SignalRApp
+{
+    public class ChatHub : Hub
+    {
+        public void HerkeseGonder(string gonderen,string mesaj)
+        {
+            Clients.All.herkeseGonder(gonderen, mesaj,$"{DateTime.Now:g}");
+        }
+    }
+}
