3bcf6c5be4bc8ebd0ae6e212874c70484dd7b227
Samet Alabey <alabeysamet@gmail.com>
2019-03-04 14:43:43 +0300

    Kişiye özel mesaj gönderme ok.

--- SignalRApp/ChatHub.cs
+++ SignalRApp/ChatHub.cs
@@ -1,13 +1,53 @@
 ﻿using Microsoft.AspNet.SignalR;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SignalRApp
 {
     public class ChatHub : Hub
     {
-        public void HerkeseGonder(string gonderen,string mesaj)
+        public static List<User> UserList = new List<User>();
+        public void HerkeseGonder(string gonderen, string mesaj)
         {
-            Clients.All.herkeseGonder(gonderen, mesaj,$"{DateTime.Now:g}");
+            Clients.All.herkeseGonder(gonderen, mesaj, $"{DateTime.Now:g}");
         }
+
+        public void OzelMesaj(string gonderenId, string aliciId, string mesaj)
+        {
+            Clients.Client(aliciId).mesajGeldi(gonderenId, mesaj);
+        }
+
+        public void Login(string kullaniciAdi, string id)
+        {
+            var giris = UserList.FirstOrDefault(x => x.Id == id);
+            giris.UserName = kullaniciAdi;
+
+            Clients.All.users(UserList);
+        }
+
+        public override Task OnConnected()
+        {
+            UserList.Add(new User()
+            {
+                Id = Context.ConnectionId
+            });
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            var silinecek = UserList.FirstOrDefault(x => x.Id == Context.ConnectionId);
+            UserList.Remove(silinecek);
+            Clients.All.users(UserList);
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+
+    public class User
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
     }
 }
