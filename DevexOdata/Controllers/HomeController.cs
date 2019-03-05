3928b9e0240dba71724cb4b521d14a547f3a2975
Samet Alabey <alabeysamet@gmail.com>
2019-03-05 09:59:50 +0300

    MockData ok.

--- DevexOdata/Controllers/HomeController.cs
+++ DevexOdata/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using DevexOdata.Models;
+using System.Web.Mvc;
 
 namespace DevexOdata.Controllers
 {
@@ -13,6 +14,22 @@ namespace DevexOdata.Controllers
         {
             ViewBag.Message = "Your application description page.";
 
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            for (int i = 0; i < 5000; i++)
+            {
+                db.Customers.Add(new Customer
+                {
+                    Address = FakeData.PlaceData.GetAddress(),
+                    Name = FakeData.NameData.GetFirstName(),
+                    Surname = FakeData.NameData.GetSurname(),
+                    Balance = FakeData.NumberData.GetNumber(1250, 999999),
+                    Phone = "05" + FakeData.PhoneNumberData.GetPhoneNumber().Replace("-", "").Substring(0,10),
+                });
+                if (i % 100 == 0)
+                    db.SaveChanges();
+            }
+            db.SaveChanges();
             return View();
         }
 
